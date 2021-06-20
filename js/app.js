bda5e28525d2a2098e823f5eefd40cf9911907bc
Muhammad Mahmoud <Muhammad.Abdo@cis.asu.edu.eg>
2021-06-20 14:03:40 +0200

    Section Active Class
    a viable function but need some modifications

--- js/app.js
+++ js/app.js
@@ -18,11 +18,13 @@
  * 
 */
 const sections=document.querySelectorAll('section'); // select sections
+const sectionsByTag=document.getElementsByTagName('section');
 // var navSections=['Section 1','Section 2','Section 3','Section 4','Section 5'];
 const appSections=document.getElementById('navbar__list'); // select ul for the creation of li
 var fragment = document.createDocumentFragment(); 
 var appLiSections; // variable to loop with on li anchor create
 var attArr=[]; //array for the store of section data to add it to the anchor of the navbar
+var aqueries;
 /**
  * End Global Variables
  * Start Helper Functions
@@ -59,7 +61,7 @@ function navApp(sections) {
     for(var i=0;i<appLiSections.length;i++){
         
         var a=document.createElement('a');
-        // var val=section.getAttributeNode("data-nav").value;
+        a.classList.add('anchorAtt');
         a.textContent=attArr[i];
         a.href='#';
         
@@ -75,9 +77,33 @@ function navApp(sections) {
 
 // Add class 'active' to section when near top of viewport
 
+function toggleActiveState(){
+    for (const section of sections){
+        section.classList.remove('your-active-class');
+    }
+    if(!!window.IntersectionObserver){    
+    let observer = new IntersectionObserver((entries, observer) => { 
+        entries.forEach(entry => {
+        if(entry.isIntersecting){
+          //console.log(entry);
+          entry.target.classList.add('your-active-class');
+          observer.unobserve(entry.target);
+        }
+        },{rootMargin: "0px 0px -200px 0px"},{threshold: 1});
+      });
+      sections.forEach(section => { observer.observe(section) });
+    }
+    else{
+        console.log("Doesn't support observe");
+    }
+}
+
 
 // Scroll to anchor ID using scrollTO event
-
+function scrollToSection(event) {
+    event.preventDefault();
+    //selected section
+}
 
 /**
  * End Main Functions
@@ -86,9 +112,9 @@ function navApp(sections) {
 */
 
 // Build menu 
-
+document.addEventListener('DOMContentLoaded',navApp(sections));
 // Scroll to section on link click
-
+//navLink.addEventListener('click', scrollToSection);
 // Set sections as active
-
+window.addEventListener('scroll', toggleActiveState);
 
