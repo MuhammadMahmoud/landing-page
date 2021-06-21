54605b91572a030804ce574e44af589cb5373b48
Muhammad Mahmoud <Muhammad.Abdo@cis.asu.edu.eg>
2021-06-21 21:20:38 +0200

    Modification after review
    -Got rid of commented code.
    -Made the js code in a beautiful shape for the eyes.
    -Changed var for const & let ES6.
    -switched toggleActiveState into An arrow function expression

--- js/app.js
+++ js/app.js
@@ -11,31 +11,30 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+ */
 
 /**
  * Define Global Variables
  * 
-*/
-const sections=document.querySelectorAll('section'); // select sections
-const sectionsByTag=document.getElementsByTagName('section');
-// var navSections=['Section 1','Section 2','Section 3','Section 4','Section 5'];
-const appSections=document.getElementById('navbar__list'); // select ul for the creation of li
-var fragment = document.createDocumentFragment(); 
-var appLiSections; // variable to loop with on li anchor create
-var attArr=[]; //array for the store of section data to add it to the anchor of the navbar
-var aqueries; // for list of anchors
-var queries; //for h2 inside sections
-var navLink=document.getElementById('navbar__menu');
+ */
+const sections = document.querySelectorAll('section'); // select sections
+const sectionsByTag = document.getElementsByTagName('section');
+const appSections = document.getElementById('navbar__list'); // select ul for the creation of li
+let fragment = document.createDocumentFragment();
+let appLiSections; // variable to loop with on li anchor create
+let attArr = []; //array for the store of section data to add it to the anchor of the navbar
+let aqueries; // for list of anchors
+let queries; //for h2 inside sections
+let navLink = document.getElementById('navbar__menu');
 // globalizing the current section & anchor to not lose the values 
-var currentSection=-10; 
-var currentAnchor='';
+let currentSection = -10;
+let currentAnchor = '';
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
-*/
+ */
 
 
 
@@ -43,121 +42,117 @@ var currentAnchor='';
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
+ */
 
 // build the nav
 function navApp(sections) {
-    var counter =0; // counter for the arr of sections data
+    let counter = 0; // counter for the arr of sections data
     // create li + class "liAtt" + store the data-nav att in array
     for (const section of sections) {
-        var li = document.createElement('li');
-        
-        var att=document.createAttribute("class");
-        att.value="liAtt";
+        let li = document.createElement('li');
+
+        let att = document.createAttribute("class");
+        att.value = "liAtt";
         li.setAttributeNode(att);
         fragment.appendChild(li);
-        attArr[counter]=section.getAttributeNode("data-nav").value;
+        attArr[counter] = section.getAttributeNode("data-nav").value;
         counter++;
     }
-    counter=0;
+    counter = 0;
     //append li
     appSections.appendChild(fragment);
-    appLiSections=document.getElementsByClassName('liAtt');
+    appLiSections = document.getElementsByClassName('liAtt');
     //create anchors for each li 
-    for(var i=0;i<appLiSections.length;i++){
-        
-        var a=document.createElement('a');
-        //a.classList.add('anchorAtt');
+    for (let i = 0; i < appLiSections.length; i++) {
+
+        let a = document.createElement('a');
         a.classList.add('menu__link');
-        a.textContent=attArr[i];
-        a.href='#';
-        
-         
-        
-        fragment.appendChild(a);           
-        
-        
-    
+        a.textContent = attArr[i];
+        a.href = '#';
+
+
+
+        fragment.appendChild(a);
+
+
+
         appLiSections[i].appendChild(fragment);
     }
-    aqueries=document.getElementsByClassName('menu__link');
+    aqueries = document.getElementsByClassName('menu__link');
 }
 
 // Add class 'active' to section when near top of viewport
 
-function toggleActiveState(){
-     //   intersect every the window meet a section
-    if(!!window.IntersectionObserver){    
-    let observer = new IntersectionObserver((entries, observer) => { 
-        entries.forEach(entry => {
-        if(entry.isIntersecting){
-            if(!(entry.target.classList.contains('your-active-class'))){
+toggleActiveState = ()=> {
+    //   intersect every the window meet a section
+    if (!!window.IntersectionObserver) {
+        let observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    if (!(entry.target.classList.contains('your-active-class'))) {
 
-                entry.target.classList.add('your-active-class');
-                //load the current intersected section & anchor to activte them
-                currentSection=entry.target.getAttribute('data-nav');
-                currentAnchor=currentSection;
-                for (const section of sections)
-                {  
-                    if(currentSection != section.getAttribute('data-nav')){
-                        section.classList.remove('your-active-class');
+                        entry.target.classList.add('your-active-class');
+                        //load the current intersected section & anchor to activte them
+                        currentSection = entry.target.getAttribute('data-nav');
+                        currentAnchor = currentSection;
+                        for (const section of sections) {
+                            if (currentSection != section.getAttribute('data-nav')) {
+                                section.classList.remove('your-active-class');
+                            }
+                        }
+                        for (const aquery of aqueries) {
+
+                            if (currentAnchor != aquery.textContent && currentAnchor != '') {
+                                aquery.id = '';
+                            } else {
+                                aquery.id = 'anchorAtt';
+                            }
+                        }
                     }
+
+                    observer.unobserve(entry.target);
+
                 }
-                for(const aquery of aqueries){
-                    //console.log( 'currentAnchor is '+ currentAnchor);
-                    if(currentAnchor != aquery.textContent && currentAnchor != ''){
-                        aquery.id='';
-                    }
-                    else{
-                        aquery.id='anchorAtt';
-                    }
-                }
-            }
-
-          observer.unobserve(entry.target);
-
-        }
-        },{rootMargin: "0px 0px px 0px"},{threshold: 1});
-      });
-      sections.forEach(section => { observer.observe(section) });
-    }
-    else{
+            }, {
+                rootMargin: "0px 0px px 0px"
+            }, {
+                threshold: 1
+            });
+        });
+        sections.forEach(section => {
+            observer.observe(section)
+        });
+    } else {
         console.log("Doesn't support observe");
     }
 }
 
 
 // Scroll to anchor ID using scrollTO event
-// function scrollToSection(event ,aquery) {
-//     
-//     event.preventDefault();
-//     for (const section of sections) {
-//         if(section.getAttributeNode("data-nav").value == aquery.textContent){
-//             section.scrollIntoView({behavior: 'smooth', block: 'center'}); 
-//         }        
-//     }
 
-    
-// }
 
 
 /**
  * End Main Functions
  * Begin Events
  * 
-*/
+ */
 
 // Build menu 
-document.addEventListener('DOMContentLoaded',navApp(sections));
+document.addEventListener('DOMContentLoaded', navApp(sections));
 for (const aquery of aqueries) {
-    aquery.addEventListener('click', function scrollToSection(event){
-    event.preventDefault();
-    for (const section of sections) {
+    aquery.addEventListener('click', function scrollToSection(event) {
+        event.preventDefault();
+        for (const section of sections) {
             //scrol into the end of the section to avoid intersecting with the next section
-        if(section.getAttribute('data-nav') == aquery.textContent){
-            section.scrollIntoView({behavior: 'smooth', block: 'end', inline: "nearest"}); 
-        }        
-    }
+            if (section.getAttribute('data-nav') == aquery.textContent) {
+                section.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'end',
+                    inline: "nearest"
+                });
+            }
+        }
     });
 }
 // Set sections as active
